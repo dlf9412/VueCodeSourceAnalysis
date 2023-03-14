61bd48011d37893116df01fe2bada2bee7fddf5a
denglifang <1055205416@qq.com>
2023-03-15 07:38:23 +0800

    注释

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -1,8 +1,11 @@
 /* @flow */
 
 import { mergeOptions } from '../util/index'
-
+/**
+ * 定义Vue.mixin,负责全局混入选项，影响之后所有创建的Vue实例，这些实例会合并全局混入的选项
+ */
 export function initMixin (Vue: GlobalAPI) {
+  // 在Vue 的默认配置项上合并mixin对象
   Vue.mixin = function (mixin: Object) {
     this.options = mergeOptions(this.options, mixin)
     return this
